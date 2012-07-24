438974aed1c740e229d2fd8537aea3b5f29cae4a
galstrom21 <jshepher@rackspace.com>
2012-07-23 21:55:54 -0500

    Ensuring copyright banner exists

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Rackspace Hosting, Inc."
 license          "Apache 2.0"
-description      "Installs/Configures monitoring"
+description      "Abstraction layer for monitoring via multiple providers"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
 
