9378d471fce9cbe9013af991e59694ae09c28706
rcbjenkins <rcb-deploy@lists.rackspace.com>
2012-10-31 14:30:19 -0500

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Rackspace US, Inc."
 license          "Apache 2.0"
 description      "Abstraction layer for monitoring via multiple providers"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.6"
+version          "0.0.7"
 
 %w{ubuntu fedora}.each do |os|
   supports os
